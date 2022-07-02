c9ffcb3e19acb0a7f20d755869c6d3547ff27af3
HECTORCAPOBLANCO <cancer18ar@hotmail.com>
2022-07-02 12:30:27 -0300

    header avanzado

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const path = require('path')
 
 const app = express(); 
 
-app.use('/static', express.static(path.resolve(__dirname + '/public')));
+app.use( express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname + '/views/index.html'))); 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
+
+app.listen(3000, ()=>{   console.log('Servidor esta corriendo');
 });
