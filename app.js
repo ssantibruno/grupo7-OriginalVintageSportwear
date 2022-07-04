504cc1207253dec7175341aeaa4e6848bc36cb5a
NikkoA <nicolasacosta1987@gmail.com>
2022-07-03 23:23:06 -0300

    Creando Login

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const app = express();
 
 app.use( express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname + '/views/index.html'))); 
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname + '/views/index.html')));
+app.get('/product', (req, res) => res.sendFile(path.resolve(__dirname + '/views/productDetail.html')));
+app.get('/car', (req, res) => res.sendFile(path.resolve(__dirname + '/views/productCar.html')));
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname + '/views/register.html')));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname + '/views/login.html'))); 
 
 app.listen(3000, ()=>{   console.log('Servidor esta corriendo');
 });
