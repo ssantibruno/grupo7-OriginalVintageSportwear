ebfcacca541ba6c86f0d632ac94e681f7c822634
HECTORCAPOBLANCO <cancer18ar@hotmail.com>
2022-07-08 11:55:05 -0300

    botones de enlace y tamaño de baner y logo

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.get('/product', (req, res) => res.sendFile(path.resolve(__dirname + '/views/
 app.get('/car', (req, res) => res.sendFile(path.resolve(__dirname + '/views/productCar.html')));
 app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname + '/views/register.html')));
 app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname + '/views/login.html'))); 
+app.get('/Selecciones', (req, res) => res.sendFile(path.resolve(__dirname + '/views/Selecciones.html')));
+app.get('/Resto-del-mundo', (req, res) => res.sendFile(path.resolve(__dirname + '/views/Resto-del-mundo.html'))); 
+app.get('/Equipos-Americanos', (req, res) => res.sendFile(path.resolve(__dirname + '/views/Equipos-Americanos.html'))); 
+app.get('/Equipos-Europeos', (req, res) => res.sendFile(path.resolve(__dirname + '/views/Equipos-Europeos.html'))); 
+app.get('/index', (req, res) => res.sendFile(path.resolve(__dirname + '/views/index.html'))); 
 
 app.listen(3000, ()=>{   console.log('Servidor esta corriendo');
 });
